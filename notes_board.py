5480babaab683cbf4e2929b406811f3ac87c039a
Broken Inteligence <98245225+rexed02@users.noreply.github.com>
2024-10-08 01:09:13 -0400

    Add files via upload

--- notes_board.py
+++ notes_board.py
@@ -6,17 +6,19 @@ class NotesBoardDocker(DockWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Notes Board")
-
         main_widget = QWidget(self)
         layout = QVBoxLayout(main_widget)
-
+        
         self.status_label = QLabel("Status: Inactive")
         layout.addWidget(self.status_label)
-
+        
         self.toggle_button = QPushButton("Activate Notes Board")
         self.toggle_button.clicked.connect(self.toggle_notes_board)
         layout.addWidget(self.toggle_button)
-
+        
+        self.ask_before_accept_checkbox = QCheckBox("Ask before create a note")
+        layout.addWidget(self.ask_before_accept_checkbox)
+        
         self.setWidget(main_widget)
         self.is_active = False
         self.notes_layer = None
@@ -54,17 +56,26 @@ class NotesBoardDocker(DockWidget):
             self.status_label.setText("Error: No active document or notes layer")
             return
 
-        result = QMessageBox.question(None, "Deactivate Notes Board", "¿Do you want to contiunue?", QMessageBox.Yes | QMessageBox.No)
-        if result == QMessageBox.Yes:
-            # Copiar la capa de notas como referencia y luego eliminarla
-            self.copy_as_reference(self.notes_layer)
-            doc.rootNode().removeChildNode(self.notes_layer)
-            self.notes_layer = None
-            self.status_label.setText("Status: Inactive")
-            self.is_active = False
-            self.toggle_button.setText("Activate Notes Board")
+        if not self.ask_before_accept_checkbox.isChecked():
+            self.process_deactivation()
         else:
-            doc.setActiveNode(self.notes_layer)
+            result = QMessageBox.question(None, "Deactivate Notes Board", "¿Do you want to continue?", QMessageBox.Yes | QMessageBox.No)
+            if result == QMessageBox.Yes:
+                self.process_deactivation()
+            else:
+                doc.setActiveNode(self.notes_layer)
+
+    def process_deactivation(self):
+        app = Krita.instance()
+        doc = app.activeDocument()
+        
+        # Copiar la capa de notas como referencia y luego eliminarla
+        self.copy_as_reference(self.notes_layer)
+        doc.rootNode().removeChildNode(self.notes_layer)
+        self.notes_layer = None
+        self.status_label.setText("Status: Inactive")
+        self.is_active = False
+        self.toggle_button.setText("Activate Notes Board")
 
     def copy_as_reference(self, layer):
         """Función para copiar el contenido de una capa como referencia."""
